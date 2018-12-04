8ab63cb30e23c403c2a6f0d9cdce19bbe61df9bd
mkhiwa <r21mordaunt@gmail.com>
2018-12-04 23:57:17 +0200

    make it better

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_11_27_193437) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "code"
     t.string "description"
